038f982abfa3a821ed677f2e7573ef1ffc323062
Nitish RAI <115301219+Nitishrai1@users.noreply.github.com>
2024-01-11 05:47:16 +0000

    conversion of data type

---
+++ 01_basic/conversion.js
@@ -0,0 +1,10 @@
+let score=33
+console.log(typeof score);
+console.log(typeof (score));
+
+// let valueInNumber=Number(score)  //this line garuentee that the number is a number
+console.log(valueInNumber);
+
+let someNumber = 33
+let stringNumber=String(someNumber)
+console.log(typeof stringNumber)
