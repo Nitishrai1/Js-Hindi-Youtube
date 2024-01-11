b13d83b251937be75838169ad1d7bee6dd8e6768
Nitish RAI <115301219+Nitishrai1@users.noreply.github.com>
2024-01-11 09:48:01 +0000

    dataypes and conversion

--- 01_basic/conversion.js
+++ 01_basic/conversion.js
@@ -1,10 +1,42 @@
-let score=33
-console.log(typeof score);
-console.log(typeof (score));
+// let score=33
+// console.log(typeof score);
+// console.log(typeof (score));
 
-// let valueInNumber=Number(score)  //this line garuentee that the number is a number
-console.log(valueInNumber);
+// // let valueInNumber=Number(score)  //this line garuentee that the number is a number
+// console.log(valueInNumber);
 
-let someNumber = 33
-let stringNumber=String(someNumber)
-console.log(typeof stringNumber)
+// let someNumber = 33
+// let stringNumber=String(someNumber)
+// console.log(typeof stringNumber)
+// *************************************** operations  ************************************************
+
+
+let value = 3
+let negvalue=-value
+console.log(negvalue);   //-3
+console.log(2-2);
+console.log(2+2);
+console.log(2*2);
+console.log(2**3);
+console.log(2%3);
+console.log("1"+2);
+console.log("1"+"2"+2);   //if strting is in first then all we be taken as string
+console.log(1+"2"+2);     //else first addition then string
+console.log(1+2+"2");
+
+
+console.log(null>0);
+console.log(null>=0);
+console.log(null<0);
+console.log(null==0);
+console.log(null===0);
+
+
+
+
+
+
+
+
+
+
---
+++ datatype_summary.js
@@ -0,0 +1,17 @@
+// Data Types
+// 1: primitive data types:   there are 7 categories string, Number, Boolean, null, undefined, Symbol, BigInt
+// 2: Non primitive data types  Reference Array, Objects , Functions
+// javascript is a dynamicaly typed langauge
+
+// arrays
+const heros=["saktiman","krish","superman"];
+// object
+let obj={
+    name:"Nitish",
+    age:"21",
+}
+
+// function 
+const myFunction=function(){
+    console.log("Namaste Duniya");
+}
